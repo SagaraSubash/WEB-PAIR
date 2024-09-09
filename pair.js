f8e90c874f1cf1aac61c5a3058baeee303866203
SagaraSubash <110216100+SagaraSubash@users.noreply.github.com>
2024-09-09 16:42:26 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -62,7 +62,17 @@ router.get('/', async (req, res) => {
                         const dt = await PrabathPairWeb.sendMessage(user_jid, {
                             text: sid
                         });
-
+                        const desc = `*𝙳𝚘𝚗𝚝 𝚜𝚑𝚊𝚛𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚠𝚒𝚝𝚑 𝚊𝚗𝚢𝚘𝚗𝚎!! 𝚄𝚜𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚝𝚘 𝚌𝚛𝚎𝚊𝚝𝚎 𝚅𝙰𝙹𝙸𝚁𝙰-𝙼𝙳 𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚄𝚜𝚎𝚛 𝚋𝚘𝚝.*\n\n ◦ *Github:* https://github.com/VajiraTech/VAJIRA-MD`; 
+                        await PrabathPairWeb.sendMessage(user_jid, {
+text: desc,
+contextInfo: {
+externalAdReply: {
+title: "Qᴜᴇᴇɴ-ᴢᴀᴢɪᴇ-ᴍᴅ",
+thumbnailUrl: "https://telegra.ph/file/e069027c2178e2c7475c9.jpg",
+sourceUrl: "https://whatsapp.com/channel/0029VahMZasD8SE5GRwzqn3Z",
+mediaType: 1,
+renderLargerThumbnail: true
+}}},{quoted:code })
                     } catch (e) {
                         exec('pm2 restart prabath');
                     }
